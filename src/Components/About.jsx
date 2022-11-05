25370665502e9bb79b26fe9d0bb41e294337293e
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2022-11-05 20:24:51 +0530

    edited

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,11 +7,11 @@ const About = () => {
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
-        <p className='mt-20 text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi vel fugit quas quibusdam error debitis ea officiis fuga odio. Commodi rem nobis ducimus distinctio odio nulla optio nemo eos corrupti beatae molestias voluptatum amet velit mollitia delectus itaque culpa esse modi veniam minima ab magni, ea provident sit. Quisquam, beatae!
+        <p className='mt-20 text-2xl'>An independent and self-motivated person, with proven and experienced Frontend Development Skills. Looking to secure a challenging position in a reputable company to explore a problem solving environment and expand my knowledge.
         </p>
         <br />
-        <p className='text-xl'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum velit ullam culpa eius. Quam velit tempora nobis voluptatibus maxime. Maxime, perspiciatis harum inventore cumque consequuntur porro quasi dolore illo debitis tempora consectetur ex cupiditate laudantium quam quaerat minus minima blanditiis mollitia quia laboriosam aperiam voluptates. Unde quia possimus blanditiis recusandae.
+        <p className='text-2xl'>
+            My current studies are in the MERN Full Stack Programme at Masai School, where I have learned that good apps lead to good business, and I enjoy building websites using different technology stacks.
         </p>
       </div>
     </div>
